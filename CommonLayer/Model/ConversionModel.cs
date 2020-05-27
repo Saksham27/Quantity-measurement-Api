144f1a3fd11e655cc392730224e66f29965c57b8
Saksham27 <geekysaksham27@gmail.com>
2020-05-27 09:34:43 +0530

    Added: Poco class for ConversionModel

---
+++ CommonLayer/Model/ConversionModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    /// <summary>
+    /// Model Class For Quantity.
+    /// </summary>
+    public class ConversionModel
+    {
+        public ConversionModel()
+        {
+            Id = 0;
+            Value = 0;
+            OperationType = null;
+            Result = 0;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="decimal(18,2)")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?")]
+        public decimal Value { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z][a-zA-Z]*$")]
+        public string OperationType { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?")]
+        public decimal Result { get; set; }
+    }
+}
