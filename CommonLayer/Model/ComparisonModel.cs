f8d22622cc831ed27e068193886de33d65cb80f7
Saksham27 <geekysaksham27@gmail.com>
2020-05-27 09:36:23 +0530

    Added : Poco class for comparisonModel

---
+++ CommonLayer/Model/ComparisonModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CommonLayer.Model
+{
+    public class ComparisonModel
+    {
+        public ComparisonModel()
+        {
+            ValueOne = 0;
+            ValueOneUnit = null;
+            ValueTwo = 0;
+            ValueTwoUnit = null;
+            Result = null;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?")]
+        public decimal ValueOne { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z][a-zA-Z]*$")]
+        public string ValueOneUnit { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?")]
+        public decimal ValueTwo { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z][a-zA-Z]*$")]
+        public string ValueTwoUnit { get; set; }
+
+        public string Result { get; set; }
+    }
+}
