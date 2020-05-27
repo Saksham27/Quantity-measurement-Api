585d1d904d725c691223daf1f5241994fde2148f
Saksham27 <geekysaksham27@gmail.com>
2020-05-27 09:36:59 +0530

    Added: Enumeration for comparison units

---
+++ CommonLayer/Model/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonLayer.Model
+{
+    /// <summary>
+    /// Enum For Units.
+    /// </summary>
+    public enum Unit
+    { 
+        Inch=1, Feet, Yard,
+        Gram, Kilogram, Tonne,
+        Ml, Litre,
+        C, F
+    }
+}
