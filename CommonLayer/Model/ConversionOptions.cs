c765ce18266a076e359452d956cf22cb31af58d9
Saksham27 <geekysaksham27@gmail.com>
2020-05-27 09:35:26 +0530

    Enumeration for all the conversion options

---
+++ CommonLayer/Model/ConversionOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace CommonLayer.Model
+{
+    public enum ConversionOptions 
+    {
+        // Enum For Length Conversion Operations. 
+        InchToFeet = 1, FeetToInch,
+        InchToYard, YardToInch, 
+        FeetToYard, YardToFeet,  
+
+        // Enum For Weight Conversion Operations.
+
+        GramToKg, KgToGram,
+        KgToTonne, TonneToKg,
+        GramToTonne, TonneToGram,
+
+        // Enum For Volume Conversion Operations.
+       LitreToMl, MlToLitre,
+
+        // Enum For Temperature Conversion Operations.
+        CToF, FToC
+    }
+}
