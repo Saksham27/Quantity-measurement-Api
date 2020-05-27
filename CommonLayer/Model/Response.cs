14e979c9ecf40b039d63e0d44a8eb883e7d22114
Saksham27 <geekysaksham27@gmail.com>
2020-05-27 09:37:33 +0530

    Added: Poco class for response SMD format

---
+++ CommonLayer/Model/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+            Success = false;
+            Message = null;
+            Data = new List<T>();
+        }
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public List<T> Data { get; set; }
+    }
+}
